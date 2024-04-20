a564ad4d808891ee15e712e9c1e1dc8d3f3b2697
Thomas <Thomas@JFAMILY-PC>
2024-04-20 12:46:37 -0400

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.ultimateqa.com");
+            string pageTitle = driver.Title;
+
+            Assert.AreEqual("Homepage - Ultimate QA", pageTitle);
+
+            Thread.Sleep(5000);
+            driver.Close();
+
+        }
+    }
+}
